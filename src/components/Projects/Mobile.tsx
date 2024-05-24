9350c43022f30be4ac0b601bc9efbbe5df4067d7
João <joaovic921@gmail.com>
2024-05-24 07:47:16 -0300

    new swiper mobile

--- src/components/Projects/Mobile.tsx
+++ src/components/Projects/Mobile.tsx
@@ -1,35 +1,19 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import { projectDatabase } from './projects';
 import 'swiper/css';
 import './projects.scss';
 
 export const Mobile: React.FC = () => {
-  const [currentProjectIndex, setCurrentProjectIndex] = React.useState(0);
-
-  const prevProjectHandle = () => {
-    const newIndex = currentProjectIndex - 1;
-    if(projectDatabase.hasOwnProperty(newIndex)) {
-      setCurrentProjectIndex(newIndex);
-    }
-  };
-
-  const nextProjectHandle = () => {
-    const newIndex = currentProjectIndex + 1;
-    if(projectDatabase.hasOwnProperty(newIndex)) {
-      setCurrentProjectIndex(newIndex);
-    }
-  };
-
   return (
     <section id="projects">
       <h1>Projetos que já desenvolvi</h1>
 
       <Swiper
+        modules={[Navigation, Pagination]}
         spaceBetween={50}
         slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper: any) => console.log(swiper)}
       >
         {projectDatabase.map((project: projectDatabase, _: number) => (
           <SwiperSlide key={`swiper-${_}`} className="card">
@@ -39,14 +23,6 @@ export const Mobile: React.FC = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-
-      <div className="counter-swiper">
-        {Object(projectDatabase).map((project: projectDatabase, _: number) => {
-          return (
-            <div key={`swiper-${_}`} className={`grey ${_ === currentProjectIndex ? 'active' : ''}`}></div>
-          );
-        })}
-      </div>
     </section>
   );
 };
